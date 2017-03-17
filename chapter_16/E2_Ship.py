e1f68e6a074493f658b543a6033243a8f2d3713d
Nick Espig <nickespig@gmail.com>
2017-03-17 09:13:24 +0100

    chapter_16: rewrote exercise 2

--- chapter_16/E2_Ship.py
+++ chapter_16/E2_Ship.py
@@ -1,8 +1,25 @@
 from tkinter import *
-from random import randint
+from random import *
 
 
-class App:
+class Cell:
+    def __init__(self, game, x, y):
+        self.x = x
+        self.y = y
+        self.game = game
+        self.button = Label(self.game.window, text="   ", relie="groove", width=2)
+        self.button.bind("<Button-1>", self.press)
+        self.button.grid(column=x, row=y)
+
+    def press(self, *args):
+        check = self.game.playfield.checkHit
+        if check(self.x, self.y):
+            self.button.config(bg="blue")
+        else:
+            self.button.config(bg="grey")
+
+
+class Playfield:
     ships = [
         (4, 1),  # 4 times 1x1
         (3, 2),  # 3 times 1X2
@@ -10,77 +27,56 @@ class App:
         (1, 4)  # 1 time  1x4
     ]
 
-    playfield_size = (12, 12)
+    playfield_size = 12
 
     def __init__(self):
+        self.d = {}
+        for i in range(self.playfield_size):
+            for j in range(self.playfield_size):
+                self.d[(i, j)] = 0
+        for ship in self.ships:
+            for k in range(ship[0]): self.setBoatPos(ship[1])
+
+    def setBoatPos(self, length):
+        _max = self.playfield_size - 1 - length
+        ok = 0
+
+        while not ok:
+            if choice([0, 1]):  # vertical
+                y = randint(1, self.playfield_size - 2)
+                x = randint(1, _max)
+                if self.check(x, y, x + length - 1, y):
+                    for x in range(x, x + length):
+                        self.d[(x, y)] = 1
+                    ok = 1
+            else:  # horizontal
+                x = randint(1, self.playfield_size - 2)
+                y = randint(1, _max)
+                if self.check(x, y, x, y + length - 1):
+                    for y in range(y, y + length):
+                        self.d[(x, y)] = 1
+                    ok = 1
+
+    def check(self, x1, y1, x2, y2):
+        ok = 1
+        for x in range(x1 - 1, x2 + 2):
+            for y in range(y1 - 1, y2 + 2):
+                if self.d[x, y]:
+                    ok = 0
+        return ok
+
+    def checkHit(self, x, y):
+        return self.d[x, y]
+
+
+class Game:
+    def __init__(self):
         self.window = Tk()
-        self.window.geometry("400x400")
-
-        self.playfield = [[0 for y in range(self.playfield_size[0])] for x in range(self.playfield_size[1])]
-
-        self.ship_positions = {}
-
-        #self.generate_ships()
-
-    def reset(self):
-        # self.result.config(text="")
-        pass
-
-    def pack(self):
-        # self.leftSide.pack(side=LEFT, fill=Y)
-        # self.rightSide.pack(side=RIGHT, fill=Y)
-        # self.scale.pack(fill=X)
-        # self.check.pack()
-        # self.reset.pack()
-        # self.result.pack()
-        pass
-
-    def generate_ships(self):
-        for i, ship in enumerate(self.ships):
-            self.ship_positions[i] = ""
-            while self.ship_positions[i] == "":
-                horizontal = randint(0, 1)
-                if horizontal:
-                    start_position = (randint(0, self.playfield_size[0] - ship[1]), randint(0, self.playfield_size[1]))
-                else:
-                    start_position = (randint(0, self.playfield_size[0]), randint(0, self.playfield_size[1] - ship[1]))
-
-                test = True
-                temp_position = []
-                try:
-                    if horizontal:
-                        for j in range(ship[1]):
-                            if self.playfield[start_position[0]][start_position[1] + j] == 0:
-                                temp_position[j] = (start_position[0], start_position[1] + j)
-                            else:
-                                test = False
-                        if test:
-                            self.ship_positions[i] = [temp_position]
-                        print(temp_position)
-
-                    else:
-                        for j in range(ship[1]):
-                            if self.playfield[start_position[0 + j]][start_position[1]] == 0:
-                                temp_position[j] = (start_position[0] + j, start_position[1])
-                            else:
-                                test = False
-                        if test:
-                            self.ship_positions[i] = [temp_position]
-                        print(temp_position)
-                except:
-                    print(start_position)
-                    pass
-        for ship in self.ship_positions:
-            self.playfield[ship[0]][ship[1]] = 1
-
-    def __str__(self):
-        text = ""
-        for idx, x in enumerate(self.playfield):
-            text += str(idx) + " " + str(x) + "\n"
-        return text
+        self.playfield = Playfield()
+        for x in range(self.playfield.playfield_size):
+            for y in range(self.playfield.playfield_size):
+                button = Cell(self, x, y)
+        self.window.mainloop()
 
 
-app = App()
-print(app)
-app.generate_ships()
-print(app)
+game = Game()
